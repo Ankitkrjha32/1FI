e58fc415ef0d3bfd35f0321ef246405eba2a26e0
Ankit kumar jha <ankitkrjha32@gmail.com>
2025-11-11 17:54:55 +0530

    ..

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:5000/api';
- const API_URL='https://onefi-backend-ydff.onrender.com';
+ const API_URL='https://onefi-backend-ydff.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_URL,
